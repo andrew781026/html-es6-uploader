c3c9245e99d7ea13c11afa3185922b04776b882d
王澍(Andrew) <andrewwang1@micb2b.com>
2020-05-06 12:10:29 +0800

    change browserSync.reload into gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,21 @@
 // 參考資料 : https://kejyuntw.gitbooks.io/gulp-learning-notes/plguins/Tool/Plugins-Tool-browser-sync.html
 
-const gulp        = require('gulp');
+const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 
 // 服務
-gulp.task('serve',  function () {
+gulp.task('serve', function () {
     browserSync.init({
-        baseDir: "./src"
+        server: {
+            baseDir: "src/",
+            index: "index.html"
+        },
+        files: [  // 監看檔案變化
+            {
+                match: ["src/*.html", "src/css/*.css", "src/js/*.js"],
+                fn: (event, file) => browserSync.reload() // https://stackoverflow.com/questions/42355259/browser-sync-not-reloading-for-file-event-add-change
+            }
+        ]
     });
 
-    // 監看工作
-    gulp.watch("src/*.html").on('change', browserSync.reload);
 });
